bc4051aa214cd5dd37da66129ad5dbcf05cea67e
Csaba19999 <menesi777@gmail.com>
2021-05-28 20:24:54 +0200

    ujKepek

--- newjavascript.js
+++ newjavascript.js
@@ -20,5 +20,41 @@ $(function() {
         $('main').addClass('parallax3');
         $('article').append('<div class="nude3"></div>');
     });
+    $( "#szoke" ).click(function() {
+        $('article').empty();
+        $('main').removeClass();
+        $('main').addClass('parallax4');
+        $('article').append('<div class="nude4"></div>');
+    });
+    $( "#voros2" ).click(function() {
+        $('article').empty();
+        $('main').removeClass();
+        $('main').addClass('parallax5');
+        $('article').append('<div class="nude5"></div>');
+    });
+    $( "#black" ).click(function() {
+        $('article').empty();
+        $('main').removeClass();
+        $('main').addClass('parallax6');
+        $('article').append('<div class="nude6"></div>');
+    });
+    $( "#bait" ).click(function() {
+        $('article').empty();
+        $('main').removeClass();
+        $('main').addClass('parallax7');
+        $('article').append('<div class="nude7"></div>');
+    });
+    $( "#milf1" ).click(function() {
+        $('article').empty();
+        $('main').removeClass();
+        $('main').addClass('parallax8');
+        $('article').append('<div class="nude8"></div>');
+    });
+    $( "#milf2" ).click(function() {
+        $('article').empty();
+        $('main').removeClass();
+        $('main').addClass('parallax9');
+        $('article').append('<div class="nude9"></div>');
+    });
     
 });
